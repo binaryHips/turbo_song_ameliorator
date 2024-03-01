1374756e9d12f02fb534f78e2c661586dc12ae19
tom <tomben01@laposte.net>
2024-03-01 10:09:49 +0100

    finished import

--- src/file_import.rs
+++ src/file_import.rs
@@ -1,10 +1,11 @@
 
 use fundsp::hacker32::*;
-
+use std::fs;
+use midly::Smf;
 
 /// Imports a wave file in 32 bits fixed-point format.
 /// Takes an absolute path string as an input.
-pub fn import_wav_file_32(pathstring:&str) -> Wave32 {
+pub fn import_wav32(pathstring:&str) -> Wave32 {
     let path = std::path::Path::new(&pathstring);
 
     let wave = Wave32::load(path).expect("Could not load file");
@@ -19,4 +20,21 @@ pub fn save_wav32(file:Wave32, pathstring:&str) {
     let path = std::path::Path::new(pathstring);
 
     file.save_wav32(path).expect("Could not save file");
+}
+
+
+/// loads an analysis file.
+/// Takes an absolute path string as an input.
+/// returns a tuple containing the bytes
+
+
+pub fn import_analysis_file<'a>(pathstring:&str, bytes: &'a mut Vec<u8>, smf: &mut Smf<'a>) {
+
+
+
+    // Load bytes into a buffer
+    *bytes = fs::read(pathstring).unwrap();
+
+    // Parse bytes in a separate step
+    *smf = Smf::parse(bytes).unwrap();
 }
