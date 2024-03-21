4b645fbb3a44bb68cd140f0845bbd2b0445d54a6
clement.saperes@etu.umontpellier.fr <clement.saperes@etu.umontpellier.fr>
2024-03-21 18:09:16 +0100

    first sample not finish

--- src/rythm_analyser.rs
+++ src/rythm_analyser.rs
@@ -8,18 +8,19 @@ pub fn find_wave_bpm(wave : Wave32) -> i32
     let mut correlation = 0.0;
     let sr = wave.sample_rate();
     let len = wave.len() as i32;    // nombre de sample
+    // Optimisation : remplacer le for par un parcours de liste auquel on enlève les multiples des bpm trop faible
     for test in 30..241             // le bpm qu'on test
     {
-        let mut some = 0.0;
+        let mut sum = 0.0;
         for sample in (60*(sr as i32)/test)..len        
         {
             // Les calculs des samples sont arrondies à l'inférieurs mais comme on les parcours un par un ensuite, le décallage ne se cumule pas
-            some += wave.at(0 as usize, sample as usize)*wave.at(0 as usize, (sample-(60*(sr as i32)/test)) as usize);
+            sum += wave.at(0 as usize, sample as usize)*wave.at(0 as usize, (sample-(60*(sr as i32)/test)) as usize);
         }
-        if some > correlation
+        if sum > correlation
         {
             bpm = test;
-            correlation = some;
+            correlation = sum;
         }
     }
     return bpm;
@@ -31,6 +32,16 @@ pub fn find_wave_bpm(wave : Wave32) -> i32
 /// Retourne le numéro du sample en un entier sur 32 bits
 pub fn find_wave_first_sample(wave : Wave32, bpm : i32) -> i32
 {
-    // à faire
+    let mut first = 0;
+    let mut correlation = 0.0;
+    let sr = wave.sample_rate();
+    let len = wave.len() as i32;    // nombre de sample
+    for sample in 0..(60*(sr as i32)/bpm)
+    {
+        for temp in 0..(len*bpm/60/(sr as i32))
+        {
+            // à faire
+        }
+    }
     return 0;
 }
