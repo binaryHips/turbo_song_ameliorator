70276518b7c5006ac5deafa51ef9b8ce7f5165d0
Ruiz Fannie <fannie.ruiz@etu.umontpellier.fr>
2024-03-28 14:14:45 +0100

    init

---
+++ interface/Godot/src/ecran_pistes/MenuButtonHelp.gd
@@ -0,0 +1,8 @@
+extends MenuButton
+
+
+func _ready():
+	get_popup().add_item("How does it work ?")
+	get_popup().add_item("Which algorithm to choose ?")
+	get_popup().add_item("About the options")
+	get_popup().add_item("Contact")
