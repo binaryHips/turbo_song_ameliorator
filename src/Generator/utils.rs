ccefb181143309714c94ae40c27e1ffca9a1f57e
Tom Zinck <tom.zinck@etu.umontpellier.fr>
2024-04-03 14:48:45 +0200

    added note class

--- src/Generator/utils.rs
+++ src/Generator/utils.rs
@@ -1,4 +1,34 @@
 mod utils;
+use std::fmt;
+
+enum Notes_name {A, Ad, B, C, Cd, D, Dd, E, F, Fd, G, Gd}
+
+const notes_str:Vec = vec!["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+
+///A simple musical note.
+/// 
+struct Note{
+
+    note:Notes_name,
+    octave:u8,
+    velocity:u8,
+
+}
+
+impl fmt::Display for Note{
+    //for printing
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", notes_str[self.note], self.octave)
+    }
+
+}
 
 
-enum notes {A, Ad, B, C, Cd, D, Dd, E, F, Fd, G, Gd}
+impl Note {
+
+
+    fn to_midi(&self) -> u8{
+        return 21 + note + (octave * 12);
+    }
+
+}
