3bba14e601c78a100f2fc5d765c52a868fe578ae
tom <tomben01@laposte.net>
2024-04-24 12:52:41 +0200

    tidied up file hierarchy and utils

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-mod file_import;
+mod file_import; //helper functions for importing basic files
+mod utils; //utility functions. Contains notes definitions.
+mod analyser;
+//mod generator;t
+
+
+
 
 fn main() {
     
