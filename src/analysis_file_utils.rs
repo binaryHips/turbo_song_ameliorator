9eff48cd755f912270bb12d34512dc7e42183574
tom <tomben01@laposte.net>
2024-04-26 21:57:03 +0200

    move analysis_file_utils; declared functions

--- src/analysis_file_utils.rs
+++
@@ -1,9 +0,0 @@
-use midly;
-
-
-
-fn add_note(file:&Smf, note:Note){
-
-    let note:midiMessage;
-    
-}
---
+++ src/utils/analysis_file_utils.rs
@@ -0,0 +1,26 @@
+use midly;
+
+
+
+pub fn add_note(file:&Smf, note:Note){
+
+    let note:midiMessage;
+    
+}
+
+
+
+pub fn get_bpm(file:&Smf) -> i32{
+
+
+}
+
+
+pub fn get_first_instant(file:&Smf) -> f64{
+
+
+}
+
+pub fn get_scale(file:&Smf) -> Vec<()>{
+
+}
