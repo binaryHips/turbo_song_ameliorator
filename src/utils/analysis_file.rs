901e94e5beb1176cfb8145980830bd0d8d498507
tom <tomben01@laposte.net>
2024-05-05 00:26:25 +0200

    added inner to cargo, fixed get_scale return type

--- src/utils/analysis_file.rs
+++ src/utils/analysis_file.rs
@@ -1,7 +1,34 @@
 use midly::{*, Smf};
 use crate::utils::*;
-use midly::*;
+use inner::*;
 
+#[derive(Clone)]
+struct AnalysisData{
+    bpm: usize,
+    notes: Vec< (f64, notes::Scale) >, // notes detected for each timestamp.
+    volumes: Vec< (f64, f32) > //volume
+}
+
+impl AnalysisData{
+
+    pub fn from_analysis_file(filename: &str) -> Result<AnalysisData>{
+
+
+    }
+
+    pub fn to_analysis_file(filename: &str) -> Result<AnalysisData>{
+        
+
+    }
+}
+
+
+///Parses a MIDI file containing analysis informations. 
+
+pub fn parse_analysis_file(midi:&Smf){
+
+
+}
 
 pub fn add_note(file: &Smf, note: notes::Note){
 
@@ -22,18 +49,27 @@ pub fn get_first_instant(file:&Smf) -> f64{
     return 5.0;
 }
 
-pub fn get_scale(file:&Smf) -> Vec<()>{
+
+///gets scale from a 
+pub fn get_scale(file:&Smf) -> notes::Scale{
     let mut NotesDebut = Vec::new();
     let liste = file.tracks[0];
     let mut time = 0;
-    let mut debut=0;
+    let mut debut = 0;
     for event in liste {
         let time = event.delta;
         if time == 0  || debut == 0 {
-            let message = match event.kind {
-                TrackEventKind::Midi { channel, message } => message,
-                _ => continue
-            };
+            
+            //ancien
+
+            // let message = match event.kind {
+            //     TrackEventKind::Midi { channel, message } => message,
+            //     _ => continue
+            // };
+
+            //Nouveau
+            let message = inner!(event, if  TrackEventKind::Midi, else {continue}).message;
+
             match message {
                 MidiMessage::NoteOn {key,vel} =>  NotesDebut.push(key),
                 MidiMessage::NoteOff {key,vel} => continue ,
@@ -45,5 +81,5 @@ pub fn get_scale(file:&Smf) -> Vec<()>{
             break;
         }
     }
-    return NotesDebut;
+    return notes::Scale::new(NotesDebut, NotesDebut[0]);
 }
