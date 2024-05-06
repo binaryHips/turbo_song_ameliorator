4c61aad37dd8d7f32f24f4c47b62f73db5fe148b
clement.saperes@etu.umontpellier.fr <clement.saperes@etu.umontpellier.fr>
2024-05-06 11:39:42 +0200

    markov adapté à la structure analysis_file

--- src/generator/algorithms/markov.rs
+++ src/generator/algorithms/markov.rs
@@ -2,7 +2,8 @@ use crate::utils::{notes, analysis_file};
 use midly;
 use rand::Rng;
 
-fn markov(ana_file : &midly::Smf, start_time : f64, end_time : f64) -> Vec<(notes::Note, f64, f64)>
+///
+fn markov(ana_file : &analysis_file::AnalysisData, start_time : f64, end_time : f64) -> Vec<(notes::Note, f64, f64)>
 {
     let rhythm_prob : Vec<Vec<f64>> = vec![vec![0.25,0.25,0.25,0.25],
                                            vec![0.25,0.25,0.25,0.25],
@@ -22,8 +23,8 @@ fn markov(ana_file : &midly::Smf, start_time : f64, end_time : f64) -> Vec<(note
                                          vec![1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0],
                                          vec![1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0],
                                          vec![1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0,1.0/13.0]];       // notes de A à G# + silence
-    let dureet : f64 = 1.0/((analysis_file::get_bpm(&ana_file)*60) as f64);        // durée d'un temp en seconde
-    let mut start_temp  : f64 = analysis_file::get_first_instant(&ana_file);
+    let dureet : f64 = 1.0/(ana_file.bpm*60.0);        // durée d'un temp en seconde
+    let mut start_temp  : f64 = ana_file.first_instant;
     while start_temp < start_time {start_temp += dureet;}
     let mut end_temp  : f64 = start_temp;
     while end_temp < end_time {end_temp += dureet;}
@@ -31,7 +32,7 @@ fn markov(ana_file : &midly::Smf, start_time : f64, end_time : f64) -> Vec<(note
     let list_rhythm : Vec<f64> = markov_rhythm(nbt, 4, &rhythm_prob);
     let nbn : i32 = list_rhythm.len() as i32;      // nombre de notes
     let list_notes : Vec<notes::Note> = markov_notes(nbn, &note_prob);
-    let melody : Vec<(notes::Note, f64, f64)> = construct_melody(list_rhythm, list_notes, start_temp);
+    let melody : Vec<(notes::Note, f64, f64)> = construct_melody(list_rhythm, list_notes);
     scaling(&melody, &ana_file);
     return melody;
 }
@@ -45,7 +46,7 @@ fn markov_rhythm(nbt : i32, precision : i32, tab : &Vec<Vec<f64>>) -> Vec<f64>
     while somme < nbt*precision
     {
         let proba : f64 = rng.gen::<f64>();
-        let somme_prob : f64 = 1.0;
+        let mut somme_prob : f64 = 1.0;
         let mut i : i32 = 0;
         while somme_prob > proba
         {
@@ -70,7 +71,7 @@ fn markov_notes(nbn : i32, tab : &Vec<Vec<f64>>) -> Vec<notes::Note>
     for note in 0..nbn
     {
         let proba : f64 = rng.gen::<f64>();
-        let somme_prob : f64 = 1.0;
+        let mut somme_prob : f64 = 1.0;
         let mut i : i32 = 0;
         while somme_prob > proba
         {
@@ -100,10 +101,10 @@ fn markov_notes(nbn : i32, tab : &Vec<Vec<f64>>) -> Vec<notes::Note>
 }
 
 
-fn construct_melody(list_rhythm : Vec<f64>, list_notes : Vec<notes::Note>, start_temp  : f64) -> Vec<(notes::Note, f64, f64)>
+fn construct_melody(list_rhythm : Vec<f64>, list_notes : Vec<notes::Note>) -> Vec<(notes::Note, f64, f64)>
 {
     let mut melody : Vec<(notes::Note, f64, f64)>;
-    let mut instant  : f64 = start_temp;
+    let mut instant  : f64 = 0.0;
     for i in 0..list_rhythm.len()
     {
         if list_notes[i].velocity != 0 {melody.push((list_notes[i], instant, instant+list_rhythm[i]));}
@@ -113,7 +114,7 @@ fn construct_melody(list_rhythm : Vec<f64>, list_notes : Vec<notes::Note>, start
 }
 
 
-fn scaling(melody : &Vec<(notes::Note, f64, f64)>, anafile : &midly::Smf)
+fn scaling(melody : &Vec<(notes::Note, f64, f64)>, anafile : &analysis_file::AnalysisData)
 {
-    for song in melody {song.0.quantize_to_scale(analysis_file::get_scale(&anafile));}
+    for song in melody {song.0.quantize_to_scale(anafile.scale);}
 }
