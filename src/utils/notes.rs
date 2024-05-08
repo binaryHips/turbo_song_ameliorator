a5046e8a426f8b9ef4ba8932585d8b9046101a3a
tom <tomben01@laposte.net>
2024-05-08 11:36:09 +0200

    fixed quantize to scale

--- src/utils/notes.rs
+++ src/utils/notes.rs
@@ -2,9 +2,15 @@ use std::fmt;
 use num_derive::FromPrimitive;    
 use num_traits::FromPrimitive;
 use midly::num;
-
+use std::cmp::min;
 #[derive(FromPrimitive, Copy, Clone, Debug)]
 pub enum NoteNames {A, Ad, B, C, Cd, D, Dd, E, F, Fd, G, Gd, SILENCE}
+
+impl From<i32> for NoteNames {
+    fn from(n: i32) -> Self {
+        FromPrimitive::from_i32((n) % 12).unwrap()
+    }
+}
 
 const NOTES_STR:[&str; 13] = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "silence"];
 
@@ -25,15 +31,15 @@ impl fmt::Display for Note{
     }
 }
 
-impl From<i32> for NoteNames {
-    fn from(n: i32) -> Self {
-        FromPrimitive::from_i32((n) % 12).unwrap()
-    }
-}
+
 
 
 impl Note {
 
+    pub fn new(note:NoteNames) -> Self{
+
+        Self { note, octave:5, velocity:num::u7::new(100)}
+    }
 
     pub fn to_midi(&self) -> num::u7{
         return num::u7::new((21 + self.note as usize + (self.octave as usize * 12)) as u8);
@@ -41,12 +47,14 @@ impl Note {
 
     pub fn quantize_to_scale(&mut self, scale:&Scale){
         let mut closest:usize = 99999;
+        let mut note:NoteNames;
         for n in &scale.notes{
             let d = (*n as usize).abs_diff(self.note as usize);
+            let d = min(d, 12-d); //modulo dist
             if d <= closest{
 
                 closest = d;
-                self.note = *n;
+                self.note = n.clone();
             }
         }
 
